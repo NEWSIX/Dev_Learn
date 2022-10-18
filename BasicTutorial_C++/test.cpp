047e3ed7a224bbf87e911f3d8e4a225f0ef89f9f
NEWSIX <nitigon.phad@gmail.com>
2022-10-18 18:17:03 +0700

    summation

--- BasicTutorial_C++/test.cpp
+++ BasicTutorial_C++/test.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << "Hello World! NEWSIX";
-  return 0;
+int main ()
+{
+int c,C=0;
+int b;
+int i = 0;
+
+
+for ( i = 0; i < 5; i++)
+{
+  cout<< "_____\nItem :"<<i+1<< "of 5\n";
+  cout << "Price :\t";
+  cin >> c ;
+  C=C+c; 
 }
+cout <<"\nTotal:\t" <<C <<"$\n";
+
+}
+
+
