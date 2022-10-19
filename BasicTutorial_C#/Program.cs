dbce74b8f556e45f035416f8d0ba79a8b064900a
NEWSIX <nitigon.phad@gmail.com>
2022-10-19 16:31:00 +0700

    Phase2

---
+++ BasicTutorial_C#/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*Console.WriteLine("Hello World! NEWSIX");    
+      Console.WriteLine("Hello World! NEWSIX");
+      int[,] A = {{12,1},{32,90},{32,90}};
+      int i=0 ;
+      
+      foreach(int n in A){
+        i++;
+        Console.WriteLine("\n {0}",n,i);
+     
+      }
+      Console.WriteLine("\n++++\t{0}\n",A.Length);*/
+    //Console.ReadKey();
+
+    string[][] NEWSIX = new string[3][];
+    NEWSIX[2] = new string[]{"NEW","SIX","NA","JA"};
+    Console.WriteLine("\n{0}\n",NEWSIX[2][1]);
+
+    }
+  }
+  class Program2
+  {
+    static void Main(string[] args)
+    {
+      /*Console.WriteLine("Hello World! NEWSIX");    
+      Console.WriteLine("Hello World! NEWSIX");
+      int[,] A = {{12,1},{32,90},{32,90}};
+      int i=0 ;
+      
+      foreach(int n in A){
+        i++;
+        Console.WriteLine("\n {0}",n,i);
+     
+      }
+      Console.WriteLine("\n++++\t{0}\n",A.Length);*/
+    //Console.ReadKey();
+
+    string[][] NEWSIX = new string[3][];
+    NEWSIX[2] = new string[]{"NEW","SIX","NA","JA"};
+    Console.WriteLine("\n{0}\n",NEWSIX[2][1]);
+
+    }
+  }
+}
