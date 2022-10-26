12ebf48572efc491dba3fd73fbbc51ba7ed8fbe5
NEWSIX <nitigon.phad@gmail.com>
2022-10-26 15:27:55 +0700

    OOP

---
+++ BasicTutorial_C#/Basic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try{
+        Console.WriteLine("Enter number:");
+        string input = Console.ReadLine();
+        int num = 100/int.Parse(input);
+        Console.WriteLine("Result is: " + num);
+      }
+      catch(FormatException){
+        Console.WriteLine("\nError 0!\n");
+      }
+      catch(Exception){
+        Console.WriteLine("\nError Other.\n");
+      }
+      finally{
+        
+      }
+
+      
+    }
+  }
+}
---
+++ BasicTutorial_C#/OOP/OOP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Console.WriteLine(employee1);
+        }
+    }
+}
