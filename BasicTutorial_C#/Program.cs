4dc42f7c8833345a99e3f136b1b7fade25c049ac
NEWSIX <nitigon.phad@gmail.com>
2022-10-26 09:48:23 +0700

    structure&enum

--- BasicTutorial_C#/Program.cs
+++ BasicTutorial_C#/Program.cs
@@ -18,13 +18,22 @@ using System;
     Console.WriteLine(b);
     Console.WriteLine(c);
   }
-
+  }
+  enum Level{
+    lv1,lv2,lv3,lv4,lv5
   }
   class main{
-    static void Main(string[] agrs){
-    ValueStr V1 = new ValueStr();
-    V1.SetValue(12,true,"12312s");
-    V1.ShowValue();
+    static void Main(string[] agrs){  
+
+      Console.WriteLine((int)Level.lv1);
+      
+      ValueStr[] V2 = new ValueStr[3];
+
+      V2[0].SetValue(1,true,"NO1");
+      V2[1].SetValue(2,true,"NO2s");
+      V2[2].SetValue(3,true,"NO3s");
+
+      //V2[0].ShowValue();
 
     }
   }
