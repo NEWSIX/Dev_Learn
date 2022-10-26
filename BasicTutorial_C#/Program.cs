4f7054760de99d5ec269f7cffc21a3afe33576c7
NEWSIX <nitigon.phad@gmail.com>
2022-10-26 10:31:06 +0700

    exception , try catch finally

--- BasicTutorial_C#/Program.cs
+++ BasicTutorial_C#/Program.cs
@@ -1,39 +1,28 @@
-﻿//using Internal;
-using System.Collections.Generic;
-using System;
+﻿using System;
 
-
-  struct ValueStr
+namespace MyApplication
+{
+  class Program
   {
-  public int a ;
-  public bool b ;
-  public string c ;  
-  public void SetValue (int A,bool B , string C ){
-    a = A;
-    b = B;
-    c = C;
-  }
-  public void ShowValue (){
-    Console.WriteLine(a);
-    Console.WriteLine(b);
-    Console.WriteLine(c);
-  }
-  }
-  enum Level{
-    lv1,lv2,lv3,lv4,lv5
-  }
-  class main{
-    static void Main(string[] agrs){  
+    static void Main(string[] args)
+    {
+      try{
+        Console.WriteLine("Enter number:");
+        string input = Console.ReadLine();
+        int num = 100/int.Parse(input);
+        Console.WriteLine("Result is: " + num);
+      }
+      catch(FormatException){
+        Console.WriteLine("\nError 0!\n");
+      }
+      catch(Exception){
+        Console.WriteLine("\nError Other.\n");
+      }
+      finally{
+        
+      }
 
-      Console.WriteLine((int)Level.lv1);
       
-      ValueStr[] V2 = new ValueStr[3];
-
-      V2[0].SetValue(1,true,"NO1");
-      V2[1].SetValue(2,true,"NO2s");
-      V2[2].SetValue(3,true,"NO3s");
-
-      //V2[0].ShowValue();
-
     }
   }
+}
