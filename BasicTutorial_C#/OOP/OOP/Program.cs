7ecc5b9a1b0c586ab6ba7616659b423f9ccac3f8
NEWSIX <nitigon.phad@gmail.com>
2022-10-26 15:36:24 +0700

    OOP inheritance

--- BasicTutorial_C#/OOP/OOP/Program.cs
+++ BasicTutorial_C#/OOP/OOP/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-
 namespace OOP
 {
     class Program
     {
+        private string a;
         static void Main(string[] args)
         {
             Employee employee1 = new Employee();
-            Console.WriteLine(employee1);
-            Console.WriteLine(employee1);
+            //Console.WriteLine(employee1);
         }
     }
 }
