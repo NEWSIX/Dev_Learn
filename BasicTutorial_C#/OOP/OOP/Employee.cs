afa7c932e217d0e52959050f8da3273d472f7078
NEWSIX <nitigon.phad@gmail.com>
2022-10-27 09:31:01 +0700

    C# OOP Login and reset Username & Password

--- BasicTutorial_C#/OOP/OOP/Employee.cs
+++ BasicTutorial_C#/OOP/OOP/Employee.cs
@@ -3,19 +3,52 @@ namespace OOP
 {
     public class Employee{
 
-        private string name = "NEWSIX";
-        private int cost = 99921;
-        private int token = 1002912;
+        private string username = "Admin";
+        private string password = "123admin";
+        private string token = "321seq";
 
-        public Employee(){
-            Console.WriteLine("\nNEWSIX IS HERE !\n"+this.token);
-        }
-
-        public Employee(string a,int b , int c)
+        public string Login(string username, string password)
         {
-            if (a == "NEWSIX" && b == 12 && c == token) { Console.WriteLine(true); }
-            else Console.WriteLine(false);
-           
+            if(this.username == username && this.password == password){
+                return "Result :\tLogin Pass!\n";
+            }
+            else { return "Result :\tLogin Failed.\n";  }
+            
+        }
+        public string ChangeUsername(string username, string newUsername)
+        {
+            if (this.username == username)
+            {
+                this.username = newUsername;
+                return "Result :\tUsername Changed.\n";
+            }
+            else { return "Result :\tInvalid.\n"; }
+
+        }
+        public string ChangePassword(string password,string newPassword)
+        {
+            if (this.password == password)
+            {
+                this.password = newPassword;
+                return "Result :\tPassword Changed.\n";
+            }
+            else { return "Result :\tInvalid.\n"; }
+
+        }
+        public string ChangeUsernameAndPassword(string token,string newUsername, string newPassword)
+        {
+            if (this.token == token)
+            {
+                this.username = newUsername;
+                this.password = newPassword;
+                return "Result :\tUsename and Password have been Changed.\n";
+            }
+            else { return "Result :\tInvalid.\n"; }
+
+        }
+        public void ShowDetails()
+        {
+            Console.WriteLine(" \t--ShowDetail--\nUSERNAME : \t{0}\nPassword : \t{1}\nToken : \t{2}", this.username, this.password, this.token);
         }
 
     }
--- BasicTutorial_C#/OOP/OOP/Program.cs
+++ BasicTutorial_C#/OOP/OOP/Program.cs
@@ -6,9 +6,26 @@ namespace OOP
         
         static void Main(string[] args)
         {
-            Employee employee2 = new Employee();
-            Employee employee1 = new Employee("NEWSIX",12,1002912);
-            Console.WriteLine("\n"+employee1+employee2);
+           
+            Employee emp1 = new Employee();
+            string login_1st = emp1.Login("Admin","123admin");
+            Console.WriteLine(login_1st);
+
+            string changePassword = emp1.ChangePassword("123admin","NEWSIX5510");
+            Console.WriteLine(changePassword);
+
+
+            string login_2nd = emp1.Login("Admin", "123admin");
+            Console.WriteLine(login_2nd);
+
+            string changeUsernameAndPassword = emp1.ChangeUsernameAndPassword("321seq", "Niti", "");
+            Console.WriteLine(changeUsernameAndPassword);
+
+            emp1.ShowDetails();
+
+
+            string login_3rd = emp1.Login("Niti","");
+            Console.WriteLine(login_3rd);
         }
     }
 }
