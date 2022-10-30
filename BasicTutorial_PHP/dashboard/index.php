b09f737a3f5eae1cb2544a5f8e93f3e7da080699
NEWSIX <nitigon.phad@gmail.com>
2022-10-30 15:28:26 +0700

    Php Basic Done!

--- BasicTutorial_PHP/dashboard/index.php
+++ BasicTutorial_PHP/dashboard/index.php
@@ -8,35 +8,74 @@
 </head>
 <body>
     <?php
+    $count = 0;
+    function cout(string $text ) {
+        $round =  $GLOBALS['count']++;
+        echo "<br>[$round] Output \t: $text <br><hr>";
+    }
+    ?>
+    <?php
+    print_r(getdate());
+
+    foreach (getdate() as $dt)
+        cout($dt);
+
+/*
+    $flt1 = 32.25;
+    $num = 22300400.052;
+
+    $res1 = ceil($flt1);
+    cout($res1);
+    $res2 = floor($flt1);
+    cout($res2);
+    $res3 = round($flt1);
+    cout($res3);
+    $res4 = number_format($num);
+    cout($res4);
+
+   // date_default_timezone_set("Asia/Bangkok");
+    cout(date("r"));
+    cout(date("d/m/Y"));
+    cout(date("d/m/Y",time()+(60*60*24*3)));
+*/
+
+/*
+    $str1 = 'NEWSIX IS ME';
+    $str2 = "NEWSIX IS ME";
+    //$str2 = "NEWSIX ";
+    $word = "HOHOHOOH";
+
+    //$res = str_replace($str2,$word,$str1);
+    //$res = strrev($str1);
+    //$res = str_repeat($str2,9999);
+    $res1 = md5($str1);
+    cout($res1);
+    $res2 = sha1($str1);
+    cout($res2);
+    $res3 = crypt($str1,$res2);
+    cout($res3);
+
+    $res4 = base64_encode($str1);
+    cout($res4);
+    
+    $res5 = base64_decode($res4);
+    cout($res5);
+*/
+    //cout($res);
    
-    $p = array(
-        array("ID"=>"k1","NO"=>"1","NAME"=>"ui"),
-        array("ID"=>"k2","NO"=>"20","NAME"=>"ssik"),
-        array("ID"=>"k3","NO"=>"33","NAME"=>"oik")
-    );
-    foreach($p as $person){
-        // print($person["ID"]."<br>");
-        // print($person["NO"]."<br>");
-        // print($person["NAME"]."<br>");
-        echo "<hr>";
-    };
+/*
+   $arr = array("Hello","world",2321);
+   $ArrToString1 = implode(" ",$arr);
+   cout($ArrToString1);
 
+   $ArrToString2 = join(" ",$arr);
+   cout($ArrToString2);
 
-    echo "<br><hr><br>";
-    $input1 = range(5,1);
-    $input2 = range("a","e");
-
-//array_splice($input1, 1, 2);
-
-$_print = array_combine($input1,$input2);
-print_r($_print);
-
- 
-  //  print_r(array_combine($input1,$input2));
-echo "<br><br>_<br><br>_<br><br>";
-echo "<br><br>_<br><br>_<br><br>";
-echo "<br><br>_<br><br>_<br><br>";
-
+   $String = "NEWSIX,2nd,1999,Thailand,Bangkok,09123XXX999";
+   $StringToArr = str_split($String,3);
+   //cout($StringToArr)
+   foreach ($StringToArr as $index)cout($index);
+   */
 
     ?>
 
