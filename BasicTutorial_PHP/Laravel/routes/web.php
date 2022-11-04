8e9afe309a5555ccb24492c17f9fe3cf215b492a
NEWSIX <nitigon.phad@gmail.com>
2022-11-04 13:54:02 +0700

    Laravel Jetsteam

--- BasicTutorial_PHP/Laravel/routes/web.php
+++ BasicTutorial_PHP/Laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        $users = User::all();
+        return view('dashboard',compact('users'));
+    })->name('dashboard');
 });
