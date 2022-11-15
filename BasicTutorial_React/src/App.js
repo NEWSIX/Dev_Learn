f631e93bdb21859af152bd7f5b006c7970e63d21
NEWSIX <nitigon.phad@gmail.com>
2022-11-15 14:26:09 +0700

    react

---
+++ BasicTutorial_React/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://avatars.githubusercontent.com/u/60424171?v=4" className="App-logo" alt="logo" />
+
+        <img  src="https://avatars.githubusercontent.com/u/60424171?v=4" className="App-logo2" alt="logo" />
+        
+        <p>
+          I'm a software engineer.
+        </p>
+        <a className="App-link" href="https://github.com/newsix" target="_blank" rel="noopener noreferrer">
+          GITHUB
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
